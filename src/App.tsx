22fcc6e14154f575d667afa7b03f755e23b4ef00
Helton-Carlos <heltoncarlospa@hotmail.com>
2022-09-04 14:01:55 -0300

    feat: ajust routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
 
-import { AppRoutes } from "./Routes"
-import { Menu } from "./layout/Menu"
+import { Index } from './pages/Index';
+import { Product } from "./pages/Product";
 
+import { Menu } from "./layout/Menu";
 function App() {
   return (
     <div className="flex">
-      <Menu />
-      <div className="m-9 w-full">
-        <AppRoutes />
-      </div>
+      <Router >
+        <Menu />
+        <div className="m-9 w-full">
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/product" element={<Product />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   )
 }
 
 export default App
+
+
+
+
--- src/layout/Menu.tsx
+++ src/layout/Menu.tsx
@@ -1,5 +1,7 @@
 import logo from '../assets/logo.svg'
-import { BeakerIcon } from '@heroicons/react/24/solid'
+import { Link } from 'react-router-dom'
+import { BsFillBookmarkDashFill, AiFillShop, FaHospitalUser, RiMoneyDollarCircleFill, AiFillCreditCard } from 'react-icons/all';
+
 
 export function Menu() {
   return (
@@ -7,9 +9,26 @@ export function Menu() {
       <div className='p-8'>
         <img src={ logo } className="logo" alt="logo" />
       </div>
-      <div className='flex items-center text-gray-500 font-medium mx-6 p-2 hover:bg-indigo-500 hover:rounded-xl hover:text-white'>
-        <BeakerIcon className="h-4 w-4" />
-        <span className='pl-2'>Dashboard</span>
+      <div>
+        <Link to="/" className='flex items-center text-gray-500 font-medium my-2 mx-6 px-4 py-2 hover:bg-indigo-500 hover:rounded-3xl hover:text-white'>
+          <BsFillBookmarkDashFill className='mr-2' /> Dashboard
+        </Link>
+
+        <Link to="/Product" className='flex items-center text-gray-500 font-medium my-2 mx-6 px-4 py-2 hover:bg-indigo-500 hover:rounded-3xl hover:text-white'>
+          <AiFillShop className='mr-2' /> Product
+        </Link>
+
+        <Link to="/" className='flex items-center text-gray-500 font-medium my-2 mx-6 px-4 py-2 hover:bg-indigo-500 hover:rounded-3xl hover:text-white'>
+          <FaHospitalUser className='mr-2' /> Customers
+        </Link>
+
+        <Link to="/" className='flex items-center text-gray-500 font-medium my-2 mx-6 px-4 py-2 hover:bg-indigo-500 hover:rounded-3xl hover:text-white'>
+          <RiMoneyDollarCircleFill className='mr-2' /> Income
+        </Link>
+
+        <Link to="/" className='flex items-center text-gray-500 font-medium my-2 mx-6 px-4 py-2 hover:bg-indigo-500 hover:rounded-3xl hover:text-white'>
+          <AiFillCreditCard className='mr-2' /> Promote
+        </Link>
       </div>
     </div>
   )
