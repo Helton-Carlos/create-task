1d6b8c9eac98c2a7b02e3fffffc9b5e7373ce523
Helton-Carlos <heltoncarlospa@hotmail.com>
2022-10-25 00:04:00 -0300

    feat: mirage cards taks

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,13 @@ export function makeServer({ environment = "test" } = {}) {
           { id: 3, title: "Work out", subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!" },
         ],
       }))
+
+      this.get("cards", () => ({
+        task: [
+          { id: 1, title: "Task", subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!" },
+          { id: 2, title: "Late conversations ", subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!" },
+        ],
+      }))
     },
   })
 
