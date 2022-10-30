513d0793f3bcd35c9e246e4e9def0ddfa394f564
Helton-Carlos <heltoncarlospa@hotmail.com>
2022-10-30 10:24:22 -0300

    feat: link logo index

--- src/layout/Menu.tsx
+++ src/layout/Menu.tsx
@@ -7,8 +7,11 @@ export function Menu() {
   return (
     <div className="bg-white w-[302px] h-screen border-2 text-center">
       <div className='p-8'>
-        <img src={ logo } className="logo" alt="logo" />
+        <Link to="/">
+          <img src={ logo } className="logo" alt="logo" />
+        </Link>
       </div>
+
       <div>
         <Link to="/" className='flex items-center text-gray-500 font-medium my-2 mx-6 px-4 py-2 hover:bg-indigo-500 hover:rounded-3xl hover:text-white'>
           <BsFillBookmarkDashFill className='mr-2' /> Dashboard
