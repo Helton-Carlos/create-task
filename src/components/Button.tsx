0bd9e6ffdf25082fbcf5a8332b45d43ebfcc9053
Helton-Carlos <heltoncarlospa@hotmail.com>
2022-11-03 22:56:09 -0300

    feat: adjust services

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,14 @@
 type TButtonProps = {
-    text?: string;
-    onClick?: any;
+  text?: string;
+  onClick?: any;
 }
 
-const MyButton: React.FC<TButtonProps> = ({ onClick, text }) => {
-    return (
-        <button className="bg-sky-700 text-white py-2 px-4 rounded-xl my-2" onClick={onClick}>
-            { text }
-        </button>
-    );
+const MyButton = ({ onClick, text }: TButtonProps) => {
+  return (
+    <button className="bg-sky-700 text-white py-2 px-4 rounded-xl my-2" onClick={onClick}>
+      {text}
+    </button>
+  );
 };
 
 export default MyButton;
--- src/service/index.ts
+++ src/service/index.ts
@@ -1,19 +1,32 @@
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs"
 
-export function makeServer({ environment = "test" }) {
-  return createServer({
+export function makeServer({ environment = "test" } = {}) {
+  let server = createServer({
     environment,
 
+    models: {
+      user: Model,
+    },
+
     routes() {
-      this.namespace = "api";
-      
-      this.get("/api/reminders", () => ({
-        reminders: [
-          { id: 1, text: "Walk the dog" },
-          { id: 2, text: "Take out the trash" },
-          { id: 3, text: "Work out" },
+      this.namespace = "api"
+
+      this.get("cards", () => ({
+        index: [
+          { id: 1, title: "good morning", subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!" },
+          { id: 2, title: "Take out the trash", subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!" },
+          { id: 3, title: "Work out", subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!" },
+        ],
+      }))
+
+      this.get("cards", () => ({
+        task: [
+          { id: 1, title: "Task", subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!" },
+          { id: 2, title: "Late conversations ", subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!" },
         ],
       }))
     },
-  });
+  })
+
+  return server
 }
